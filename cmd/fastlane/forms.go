da189991b21dda2205541e89db1d90f39e26b727
daved <daved@codemodus.com>
2025-03-05 18:16:26 -0800

    Organize main funcs

---
+++ cmd/fastlane/forms.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/charmbracelet/huh"
+)
+
+func initplayerSetupForm() *huh.Form {
+	return huh.NewForm(
+		huh.NewGroup(
+			huh.NewSelect[int]().
+				Key(playerCount).
+				Options(huh.NewOptions(1, 2, 3, 4)...).
+				Title("Players").
+				Description("Choose how many players there will be."),
+		),
+	)
+}
+
+func initCharacterSelectForm(playerCount int) *huh.Form {
+	dbg("initCharacterSelectForm start")
+	characterSelections := []*huh.Group{}
+	for i := 1; i <= playerCount; i++ {
+		characterSelections = append(characterSelections, huh.NewGroup(
+			huh.NewSelect[string]().
+				Key(fmt.Sprintf("player%d", i)).
+				Options(huh.NewOptions(john, george, ringo, paul)...).
+				Title(fmt.Sprintf("Player %d Character", i)).
+				Description(fmt.Sprintf("Choose a character for Player %d.", i)),
+		))
+	}
+
+	dbg("initCharacterSelectForm end")
+	return huh.NewForm(characterSelections...)
+}
--- cmd/fastlane/tea.go
+++ cmd/fastlane/tea.go
@@ -71,103 +71,11 @@ func (m model) Init() tea.Cmd {
 
 func InitModel() tea.Model {
 	return model{
-		viewArt:      []art.Element{art.MainTitle, "", "", "", "endGame"},
-		helpMenu:     help.New(),
-		currentState: start,
-		playerSetupForm: huh.NewForm(
-			huh.NewGroup(
-				huh.NewSelect[int]().
-					Key(playerCount).
-					Options(huh.NewOptions(1, 2, 3, 4)...).
-					Title("Players").
-					Description("Choose how many players there will be."),
-			),
-		),
+		viewArt:         []art.Element{art.MainTitle, "", "", "", "endGame"},
+		helpMenu:        help.New(),
+		currentState:    start,
+		playerSetupForm: initplayerSetupForm(),
 	}
-}
-
-func (m model) updateStart(msg tea.Msg) (tea.Model, tea.Cmd) {
-	// start game
-	switch msg := msg.(type) {
-
-	case tea.WindowSizeMsg:
-	case tea.KeyMsg:
-		switch msg.String() {
-		case "q", "ctrl+c":
-			m.quitting = true
-			return m, tea.Quit
-		default:
-			m.currentState = numOfPlayers
-		}
-	}
-
-	return m, nil
-}
-
-func (m model) updateNumOfPlayers(msg tea.Msg) (tea.Model, tea.Cmd) {
-	dbg("updateNumOfPlayers start")
-	// Process the form
-	form, cmd := m.playerSetupForm.Update(msg)
-	if f, ok := form.(*huh.Form); ok {
-		dbg("  ok")
-		m.playerSetupForm = f
-	} else {
-		dbg("  !ok")
-	}
-
-	if m.playerSetupForm.State == huh.StateCompleted {
-		// set player count and update view
-		dbg("  huh.StateCompleted")
-		m.numPlayers = m.playerSetupForm.GetInt(playerCount)
-		m.currentState = chooseCharacter
-	}
-
-	dbg("  current state: %v", m.currentState)
-	dbg("  numberOfPlayers: %d", m.numPlayers)
-	dbg("updateNumOfPlayers end")
-	return m, cmd
-}
-
-func (m model) updateCharacterSetupForm(msg tea.Msg) (tea.Model, tea.Cmd) {
-	dbg("updateCharacterSetupForm start")
-	// Process the form
-	form, cmd := m.characterSetupForm.Update(msg)
-	if f, ok := form.(*huh.Form); ok {
-		dbg("  ok")
-		m.characterSetupForm = f
-	} else {
-		dbg("  !ok")
-	}
-
-	if m.characterSetupForm.State == huh.StateCompleted {
-		dbg("  stateCompleted start")
-		// map players to characters
-		for i := 1; i < m.numPlayers+1; i++ {
-			m.playerCharacter[player(i)] = character(m.characterSetupForm.GetString(fmt.Sprintf("player%d", i)))
-		}
-		dbg("  stateCompleted end")
-		m.currentState = game
-	}
-
-	dbg("updateCharacterSetupForm end")
-	return m, cmd
-}
-
-func initCharacterSelectForm(playerCount int) *huh.Form {
-	dbg("initCharacterSelectForm start")
-	characterSelections := []*huh.Group{}
-	for i := 1; i <= playerCount; i++ {
-		characterSelections = append(characterSelections, huh.NewGroup(
-			huh.NewSelect[string]().
-				Key(fmt.Sprintf("player%d", i)).
-				Options(huh.NewOptions(john, george, ringo, paul)...).
-				Title(fmt.Sprintf("Player %d Character", i)).
-				Description(fmt.Sprintf("Choose a character for Player %d.", i)),
-		))
-	}
-
-	dbg("initCharacterSelectForm end")
-	return huh.NewForm(characterSelections...)
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
